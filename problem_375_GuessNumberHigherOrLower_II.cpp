fa3cd36cf38cfa6c450b8dab1e2d4be7c24fbf53
Hao Ye <yeahaur@gmail.com>
2021-09-26 23:52:37 -0400

    first upgrade problem 375 copied from others

---
+++ problem_375_GuessNumberHigherOrLower_II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    int recur (int start, int end, vector < vector <int> >& dp) {
+        if (start >= end) return 0;
+        if (dp[start][end] != INT_MAX) return dp[start][end];
+        for (int i = start; i <= end; i++) {
+            dp[start][end] = min(dp[start][end] ,max(i + recur(start, i - 1, dp),i + recur(i + 1, end, dp)));
+        }
+        return dp[start][end];
+    }
+    
+    int getMoneyAmount(int n) {
+        vector < vector <int> > dp(n + 1, vector <int> (n + 1, INT_MAX));
+        return recur(0, n, dp);
+    }
+};
