faa92d6200000122da40a53860a9ae5b2ae41b24
yehaur <yeahaur@gmail.com>
2021-10-06 01:17:03 -0400

    Update problem_396_RotationFunction.cpp

--- problem_396_RotationFunction.cpp
+++ problem_396_RotationFunction.cpp
@@ -14,3 +14,26 @@ public:
         return ans;
     }
 };
+// TLE above
+
+// Copy others who solve it by math 
+class Solution {
+public:
+    long long int maxRotateFunction(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            ans += i*nums[i];
+        }
+        int index=n-1;
+        int res=ans;
+        for(int k=1;k<n;k++){
+            ans = ans - ((n-1)*nums[index]) + (sum-nums[index]);  // Sum(k+1) = Sum(k) - (n-1)nums[n-1] + (sum(nums) - nums[n-1])
+            res=max(res,ans);
+            index--;
+        }
+        return res;
+    }
+};
